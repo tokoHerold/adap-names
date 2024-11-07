f11631df2d9153658078ad981da6e37b7679150d
tokoHerold <wombatwheelie@gmail.com>
2024-11-07 17:25:15 +0000

    Start implementing StringArrayName

--- src/adap-b02/names/StringArrayName.ts
+++ src/adap-b02/names/StringArrayName.ts
@@ -6,10 +6,13 @@ export class StringArrayName implements Name {
     protected delimiter: string = DEFAULT_DELIMITER;
 
     constructor(other: string[], delimiter?: string) {
+        if (other.length === 0) throw new Error("Emtpy string array not allowed!")
+        other.forEach(element => {
+            this.components.push(element);
+        });
         if (delimiter !== undefined) {
             this.delimiter = delimiter;
         }
-
     }
 
     public asString(delimiter: string = this.delimiter): string {
@@ -21,48 +24,43 @@ export class StringArrayName implements Name {
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation");
+        return this.components.length === 0;
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation");
+        return this.delimiter;
     }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation");
+        return this.components.length;
     }
 
     public getComponent(i: number): string {
-        throw new Error("needs implementation");
+        return this.components[i];
     }
 
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation");
+        if (i >= 0 && i < this.components.length)
+            this.components[i] = c;
     }
 
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation");
+        if (i >= 0 && i < this.components.length)
+            this.components.splice(i, 0, c);
     }
 
     public append(c: string): void {
-        throw new Error("needs implementation");
+        this.components.push(c);
     }
 
     public remove(i: number): void {
-        throw new Error("needs implementation");
+        if (i >= 0 && i < this.components.length)
+            this.components.splice(i, 1);
     }
 
     public concat(other: Name): void {
-        throw new Error("needs implementation");
+        for (let i = 0; i < other.getNoComponents(); i++) {
+            this.append(other.getComponent(i));
+        }
     }
-
-    /* Helper method 
-     * Checks whether a given string is correctly escaped.
-     * Returns `true` on success, false otherwise.
-     */
-    protected isValid(input : string) : boolean {
-        // TODO: implement
-        return true;
-    }
-
 }
