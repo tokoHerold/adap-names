5b1dfede6c965ade89d04261a52af4841817a798
tokoHerold <wombatwheelie@gmail.com>
2024-11-16 16:19:57 +0000

    Adapt asDataString to match new specification

--- test/adap-b03/names/Name.test.ts
+++ test/adap-b03/names/Name.test.ts
@@ -210,19 +210,19 @@ describe("Extra-Tests", () => {
 
   it("funny delimiter magic", () => {
     let nArray : Name = new StringArrayName(["o\\s\\s", "c\\s", "fau", "de"], "s");
-    expect(nArray.asDataString()).toBe("oss.cs.fau.de");
-    let nString : Name = new StringName(nArray.asDataString().replaceAll("s", "\\s").replaceAll(".", "s"), "s");
+    expect(nArray.asDataString()).toBe("o\\s\\ssc\\ssfausde");
+    let nString : Name = new StringName(nArray.asDataString(), "s");
     expect(nString.getComponent(0)).toBe("o\\s\\s");
     expect(nArray.asString()).toBe("ossscssfausde");
     expect(nString.asString()).toBe("ossscssfausde");
     expect(nString.getNoComponents()).toBe(4);
-    expect(nString.asDataString()).toBe("oss.cs.fau.de");
+    expect(nString.asDataString()).toBe("o\\s\\ssc\\ssfausde");
   });
 
   it("unfunny delimiter magic", () => {
-    let nArray : Name = new StringArrayName(["o\\.\\.", "c\\.", "fau", "de"], ".");
+    let nArray : Name = new StringArrayName(["o\\.\\.", "c\\.", "fau", "de"]);
     expect(nArray.asDataString()).toBe("o\\.\\..c\\..fau.de");
-    let nString : Name = new StringName(nArray.asDataString(), ".");
+    let nString : Name = new StringName(nArray.asDataString());
     expect(nString.asDataString()).toBe("o\\.\\..c\\..fau.de");
     expect(nArray.asString(".")).toBe("o...c..fau.de");
     expect(nString.asString(".")).toBe("o...c..fau.de");
